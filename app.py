d6bdfc6fa190a3aa22ada46c1558506ba3025732
unknown <dfsorioc@gmail.com>
2025-09-19 12:00:51 -0500

    version5

--- app.py
+++ app.py
@@ -26,6 +26,33 @@ def header():
 
     return render_template('header.html', header=header_html)
 
+
+######################################## CAPITULO 1  #########################################
+
+@app.route('/capitulo1')
+def capitulo1():
+    return render_template('capitulo1.html')
+
+@app.route('/analitica-datos')
+def analitica_datos():
+    return render_template('analitica_datos.html')
+
+@app.route('/predicciones')
+def predicciones():
+    return render_template('predicciones.html')
+
+@app.route('/modelos')
+def modelos():
+    return render_template('modelos.html')
+
+@app.route('/alertas')
+def alertas():
+    return render_template('alertas.html')
+
+@app.route('/reportes')
+def reportes():
+    return render_template('reportes.html')
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5001)
 
